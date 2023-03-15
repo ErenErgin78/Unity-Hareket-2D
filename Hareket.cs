677be9c7a661f91edff71323967e1d02e0b03da0
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2023-03-15 11:53:28 +0300

    Update Hareket.cs

--- Hareket.cs
+++ Hareket.cs
@@ -17,23 +17,22 @@ public class Hareket : MonoBehaviour
     public LayerMask LayerMask;
     private bool rb_x;
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         speed = 0.2f;
-        jumppower = 60f;
-        
-        
-        
+        jumppower = 60f;   
     }
+    
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawCube(transform.position-transform.up*distance,boxsize);
-       
     }
     
-
+    
     private void Update()
     {
         GroundCheck();
@@ -41,31 +40,26 @@ public class Hareket : MonoBehaviour
         Vertical = Input.GetAxis("Vertical");
 
 
-
         if (rb.velocity.x == 0) { rb_x = true; }
         else { rb_x = false; }
+        
 
-        if (!GroundCheck() && rb.velocity.y<=1) { rb.gravityScale = 5f; speed = 0.2f; } //düþerken
-        else if (!GroundCheck() && rb.velocity.y > 0) { speed = 0.1f; } //zýplarken
-        else if (!rb_x && GroundCheck()) { rb.gravityScale = 0f; speed = 0.3f; } //büyük zýplama 
-        else if(rb_x && GroundCheck()) { rb.gravityScale = 0f; speed = 0.3f; }  //zýplamadan hareket
+        if (!GroundCheck() && rb.velocity.y<=1) { rb.gravityScale = 5f; speed = 0.2f; } //dÃ¼Ã¾erken
+        else if (!GroundCheck() && rb.velocity.y > 0) { speed = 0.1f; } //zÃ½plarken
+        else if (!rb_x && GroundCheck()) { rb.gravityScale = 0f; speed = 0.3f; } //bÃ¼yÃ¼k zÃ½plama 
+        else if(rb_x && GroundCheck()) { rb.gravityScale = 0f; speed = 0.3f; }  //zÃ½plamadan hareket
         
        
-      
         if (Horizontal > 0.1f || Vertical < 0.1)
-        {
-            rb.AddForce(new Vector2(Horizontal * speed, 0f), ForceMode2D.Impulse);
-        }
+        { rb.AddForce(new Vector2(Horizontal * speed, 0f), ForceMode2D.Impulse); }
 
 
         if (Input.GetKey(KeyCode.Space)&&GroundCheck())
-        {
-            rb.AddForce(new Vector2(0f, jumppower), ForceMode2D.Impulse);
-        }        
+        { rb.AddForce(new Vector2(0f, jumppower), ForceMode2D.Impulse); }        
     }
+    
     private bool GroundCheck() 
     {
-
         if (Physics2D.BoxCast(transform.position, boxsize, 0, -transform.up, distance, LayerMask))
         {return true;}
         else
